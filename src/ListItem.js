5c180f9db57e53e30efbe86bcad0d053aa0e5368
jeff <jeffersondarcy@users.noreply.github.com>
2020-10-05 15:08:36 +0200

    switch color

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import PropTypes from 'prop-types';
 import Expand from "./Expand";
 
@@ -7,7 +7,7 @@ import TreeContext from "./TreeContext";
 import {toggle} from "./functions";
 
 const style = {
-    border: "1px solid green",
+    border: "1px solid",
     margin: 6,
     padding: 8,
     height: 30
@@ -15,17 +15,26 @@ const style = {
 
 const ListItem = ({id}) => {
     const [tree, setTree] = useContext(TreeContext)
+    const [color, setColor] = useState(['green'])
 
     if (!tree.has(id)) return null
 
     const self = tree.get(id)
 
     const onToggle = () => toggle(id, tree, setTree)
-
-    // const children = expanded ? getChildren(props.id) : []
+    const changeColor = () => {
+        const colors = ['green', 'blue', 'yellow', 'red', 'black']
+        setColor(colors[Math.floor(Math.random() * 5)])
+    }
 
     return (
-        <div style={{...style, marginLeft: style.margin + 30*(self.level-1)}}>
+        <div style={{
+            ...style,
+            marginLeft: style.margin + 30*(self.level-1),
+            borderColor: color,
+            }}
+            onClick={changeColor}
+        >
             <div className="ListItem">
                 {self.hasChildren
                     ? <Expand onClick={onToggle} expanded={self.expanded}/>
